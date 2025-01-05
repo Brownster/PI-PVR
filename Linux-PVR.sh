8617889a4a292636be5015fda372737c25e7120e
Marc Brown <marcsmobile.brown@googlemail.com>
2025-01-05 22:05:47 +0000

    Update Linux-PVR.sh

--- Linux-PVR.sh
+++ Linux-PVR.sh
@@ -77,54 +77,7 @@ main() {
     setup_docker_network
     deploy_docker_compose
     setup_mount_and_docker_start
-    whiptail --title "Setup Complete" --msgbox "All steps completed successfully." 10 60
-    whiptail --title "Setup Complete" --msgbox  "Setup complete. Update the .env file with credentials if not already done." 10 60
-    whiptail --title "Setup Complete" --msgbox "Setup Summary:" 10 60
-    whiptail --title "Setup Complete" --msgbox "Docker services are running:" 10 60
-
-    # Define the base URL using the server IP
-    BASE_URL="http://${SERVER_IP}"
-
-    # Define a list of services with their ports and URLs
-    declare -A SERVICES_AND_PORTS=(
-        ["VPN"]="${BASE_URL}"
-        ["Jackett"]="${BASE_URL}:${JACKET_PORT}"
-        ["Sonarr"]="${BASE_URL}:${SONARR_PORT}"
-        ["Radarr"]="${BASE_URL}:${RADARR_PORT}"
-        ["Transmission"]="${BASE_URL}:${TRANSMISSION_PORT}"
-        ["NZBGet"]="${BASE_URL}:${NZBGET_PORT}"
-        ["Get_IPlayer"]="${BASE_URL}:${GET_IPLAYER_PORT}"
-        ["JellyFin"]="${BASE_URL}:${JELLYFIN_PORT}"
-        ["Watchtower"]="(Auto-Updater - no web UI)"
-    )
-
-    # Display services and clickable URLs
-    echo "Services and their URLs:"
-    for SERVICE in "${!SERVICES_AND_PORTS[@]}"; do
-        echo "  - $SERVICE: ${SERVICES_AND_PORTS[$SERVICE]}"
-    done
-
-
-    echo "File shares available:"
-    if [[ "$SHARE_METHOD" == "1" ]]; then
-        echo "  Samba Shares:"
-        printf '    \\\\%s\\\\Movies\n' "$SERVER_IP"
-        printf '    \\\\%s\\\\TVShows\n' "$SERVER_IP"
-        printf '    \\\\%s\\\\Downloads\n' "$SERVER_IP"
-
-    elif [[ "$SHARE_METHOD" == "2" ]]; then
-        echo "  NFS Shares:"
-        echo "    $SERVER_IP:$STORAGE_DIR"
-        echo "    $SERVER_IP:$DOWNLOAD_DIR"
-
-    for SERVICE in "${!SERVICES_AND_PORTS[@]}"; do
-        echo "$SERVICE: ${SERVICES_AND_PORTS[$SERVICE]}" >> "$HOME/services_urls.txt"
-    done
-    echo "URLs saved to $HOME/services_urls.txt"
-
-
-
-    fi
+    display_summary
     generate_tailwind_html
 }
 
