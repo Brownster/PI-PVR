baf8f1956e732f74b1f6017044af8eee3330376e
Marc Brown <marcsmobile.brown@googlemail.com>
2025-01-05 21:41:17 +0000

    Update utils.sh

--- utils.sh
+++ utils.sh
@@ -50,3 +50,24 @@ write_distro_to_env() {
         echo "DISTRO=$DISTRO" >> "$ENV_FILE"
     fi
 }
+
+# Function to update /etc/fstab with the new mount point
+update_fstab() {
+    local mount_point="$1"
+    local device="$2"
+
+    # Get the UUID of the device
+    local uuid=$(blkid -s UUID -o value "$device")
+    if [[ -z "$uuid" ]]; then
+        whiptail --title "error" --msgbox   "Error: Could not retrieve UUID for device $device."
+        exit 1
+    fi
+
+    # Check if the mount point is already in /etc/fstab
+    if grep -q "$mount_point" /etc/fstab; then
+        whiptail --title "Fstab" --msgbox "Mount point $mount_point already exists in /etc/fstab. Skipping." 10 60
+    else
+        whiptail --title "Fstab" --msgbox "Adding $mount_point to /etc/fstab..."
+        echo "UUID=$uuid $mount_point auto defaults 0 2" | sudo tee -a /etc/fstab > /dev/null
+    fi
+}
