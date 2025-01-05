7f1e4b75cf9f2839cf2854df849e492c28f56562
Marc Brown <marcsmobile.brown@googlemail.com>
2025-01-05 21:24:39 +0000

    Update utils.sh

--- utils.sh
+++ utils.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+DOCKER_DIR="$HOME/docker"
+
+# Check and source the .env file
+ENV_FILE="$DOCKER_DIR/.env"
+
+if [[ -f "$ENV_FILE" ]]; then
+    source "$ENV_FILE"
+else
+    echo "Error: .env file not found at $ENV_FILE"
+    exit 1
+fi
+
 #Detect Linux Distro
 detect_distro() {
     if [ -f /etc/os-release ]; then
@@ -28,4 +40,13 @@ install_package() {
             exit 1
             ;;
     esac
+}
+
+# Write Distro to .env file
+write_distro_to_env() {
+    if grep -q "^DISTRO=" "$ENV_FILE"; then
+        sed -i "s/^DISTRO=.*/DISTRO=$DISTRO/" "$ENV_FILE"
+    else
+        echo "DISTRO=$DISTRO" >> "$ENV_FILE"
+    fi
 }
