f6b44ef35ccc11c5cf28fa01b7bb84928f32182d
Marc Brown <marcsmobile.brown@googlemail.com>
2025-01-06 20:23:15 +0000

    Update Linux-PVR.sh

--- Linux-PVR.sh
+++ Linux-PVR.sh
@@ -62,24 +62,34 @@ main() {
     if [[ -f "$ENV_FILE" ]]; then
         source "$ENV_FILE"
     fi
+    whiptail --title "Detect Distro" --msgbox "Attempt to handle multiple distibutions" 10 60
     detect_distro
     write_distro_to_env
+    whiptail --title "Install Dependencies" --msgbox "Installing required packages" 10 60
     install_dependencies
+    whiptail --title "Tailscale" --msgbox "Setting up tail scale" 10 60
     setup_tailscale
+    whiptail --title "VPN config Setup" --msgbox "Setup PIA VPN config" 10 60
     prompt_for_vpn_credentials
     setup_pia_vpn
+    whiptail --title "Docker Compose" --msgbox "Create the docker-compose file" 10 60
     create_docker_compose
     create_config_json
     ensure_docker_dir
     initial_setup_check
+    whiptail --title "Storage setup" --msgbox "Attempt to set your media folders and mount if needed" 10 60
     select_storage
     assign_folders
     create_shares
     update_env_file
+    whiptail --title "Review the Damage" --msgbox "Lets take a look at what we have done" 10 60
     final_review
+    whiptail --title "Docker Network" --msgbox "Setting up docker network" 10 60
     setup_docker_network
+    whiptail --title "Docker Compose" --msgbox "deploying docker compose" 10 60
     deploy_docker_compose
     setup_mount_and_docker_start
+    whiptail --title "Summary" --msgbox "Displaying details" 10 60
     display_summary
     generate_tailwind_html
 }
