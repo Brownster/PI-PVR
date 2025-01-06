cdec9e2ed9ff694a693a98c354f134da6b7749d3
Marc Brown <marcsmobile.brown@googlemail.com>
2025-01-06 21:05:29 +0000

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -20,10 +20,9 @@ install_dependencies() {
     whiptail --title "Install Dependancies" --msgbox "Installing dependencies..." 10 60
     install_package curl jq git
 
-    whiptail --title "Install Dependancies" --msgbox  "Uninstalling any conflicting Docker packages..." 10 60
-    for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do
-        install_package remove -y "$pkg"
-    done
+    whiptail --title "Install Dependencies" --msgbox "Removing old Docker packages..." 10 60
+    install_package remove docker.io docker-doc docker-compose podman-docker containerd runc
+
 
     whiptail --title "Install Dependancies" --msgbox  "Adding Docker's official GPG key and repository for Docker..." 10 60
     install_package ca-certificates curl
